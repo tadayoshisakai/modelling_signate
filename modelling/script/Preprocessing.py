fd3fa5bf0417bb51479fd9d220bbb43db633e7f7
t.sakai <koyo20715@yahoo.co.jp>
2023-07-27 08:32:02 +0900

    .gitignore試験

--- modelling/script/Preprocessing.py
+++ modelling/script/Preprocessing.py
@@ -3,6 +3,21 @@
 """
 Created on Tue Jul 25 07:20:22 2023
 
-@author: sakaitadayoshi
+@author: sakaitadayoshi[]
 """
 
+
+def main():
+    prep = PreProcessor()
+
+
+class PreProcessor:
+    def __init__(self):
+        print("Preprocessor: Initialize")
+
+    def test(self):
+        print("Preprocessor().test(): Initialize")
+
+
+if __name__ == "__main__":
+    main()
--- modelling/script/__main__.py
+++ modelling/script/__main__.py
@@ -1,5 +1,5 @@
 # numpy , pandas
-import numpy as np 
+import numpy as np
 import pandas as pd
 # scikit-learn
 from sklearn.preprocessing import StandardScaler
@@ -12,39 +12,119 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 import json
-
-
+import logging
+import Preprocessing as prepro
 
 
 def main():
-    trainDataFrame = pd.read_csv("../data/train.csv")
-    testDataFrame = pd.read_csv("../data/test.csv")
-    describeColumnSpecification(trainDataFrame,"train")
-    describeColumnSpecification(testDataFrame,"test")
-    print("End")
+    traindataframe = pd.read_csv("../data/train.csv")
+    testdataframe = pd.read_csv("../data/test.csv")
+    desc = DataDescriptor()
+    desc.test(traindataframe, testdataframe)
 
 
-def describeColumnSpecification(df,dataDescription="None"):
-    col = df.columns
-    reportDF=pd.DataFrame(columns=['column','type','min','max','mean','median','variation','mode'])
-    #reportDF = pd.DataFrame()
-    for i, e in enumerate(col):
-        tmpDict = {}
-        if df[e].dtype=='int64':
-            tmpDict["type"]=df[e].dtype
-            tmpDict['column']=e
-            tmpDict['min']=df[e].min()
-            tmpDict['max']=df[e].max()
-            tmpDict['mean']=round(df[e].mean(),2)
-            tmpDict['median']=df[e].median()
-        else:
-            tmpDict["type"]=df[e].dtype
-            tmpDict['column']=e
-            tmpDict['variation']=len(df[e].unique())
-            tmpDict['mode']=df[e].mode()[0]
-        reportDF = reportDF.append(tmpDict,ignore_index=True)
-    reportDF.to_csv("../result/" + dataDescription + "_columnSpecification.csv")   
- 
+class DataDescriptor:
+    def __init__(self):
+        print("DataDescriptor().__init__(): Initialize")
 
-if __name__=='__main__':
+    def describe_column_specification(self, df, datadesc="data"):
+        col = df.columns
+        mylist = []
+        for i, e in enumerate(col):
+            tmp_list = []
+            if df[e].dtype == 'int64':
+                tmp_list.append("Type:" + str(df[e].dtype))
+                tmp_list.append("Column Name:" + e)
+                tmp_list.append("min:" + str(df[e].min()))
+                tmp_list.append("MAX:" + str(df[e].max()))
+                tmp_list.append("mean:" + str(round(df[e].mean(), 2)))
+                tmp_list.append("median:" + str(df[e].median()))
+            else:
+                tmp_list.append("Type:" + str(df[e].dtype))
+                tmp_list.append("Column Name:" + e)
+                tmp_list.append("Data variation:" + str(len(df[e].unique())))
+                tmp_list.append("mode:" + str(df[e].mode()[0]))
+            mylist.append("    ".join(tmp_list))
+        print("\n".join(mylist))
+        f = open('../result/' + datadesc +
+                 '_columnSpecification.txt', 'w')
+        f.write("\n".join(mylist))
+        f.close()
+
+    def scatter_plot_multi(self, df, col, tgval, datadesc="data"):
+        print("DataDescriptor().scatter_plot_multi(): Initialize")
+        if not self._is_df_columns(df, col):
+            print("DataDescriptor().scatter_plot_multi(): elem in col is not valid")
+            return
+        for i, e in enumerate(col):
+            plt.clf()
+            plt.scatter(df[e], df[tgval])
+            plt.xlabel(e)
+            plt.ylabel(tgval)
+            plt.savefig("../figure/scatterPlot_" + datadesc + "_" + e +
+                        ".png", format="png", dpi=300)
+            # plt.show()
+
+    def dist_plot_multi(self, df, col, tgval, datadesc="data"):
+        print("DataDescriptor().dist_plot_multi(): Initialize")
+        if not self._is_df_columns(df, col):
+            print("DataDescriptor().dist_plot_multi(): elem in col is not valid")
+            return
+        for i, e in enumerate(col):
+            if df[e].dtype == 'object':
+                RANGE = [0, df[tgval].max()]
+                for cat in df[e].unique():
+                    plt.clf()
+                    plt.hist(df[tgval][df[e] == cat], range=RANGE)
+                    plt.title(datadesc + "_" + e + "_" + cat)
+                    plt.xlabel(tgval)
+                    plt.savefig("../figure/distPlot_" + datadesc + "_" + tgval + "(" +
+                                e + "=" + cat + ").png", format="png", dpi=300)
+            elif df[e].dtype == 'int':
+                plt.clf()
+                plt.hist(df[e])
+                plt.title(datadesc + "_" + e)
+                plt.xlabel(e)
+                plt.savefig("../figure/distPlot_" + datadesc +
+                            "_" + e + ".png", format="png", dpi=300)
+
+    def box_plot_multi(self, df, col, tgval, datadesc="data"):
+        print("DataDescriptor().box_plot_multi(): Initialize")
+        if not self._is_df_columns(df, col):
+            print("DataDescriptor().box_plot_multi(): elem in col is not valid")
+            return
+
+        for i, e in enumerate(col):
+            plt.clf()
+            if len(df[e].unique()) > 6:
+                sns.set(font_scale=0.5)
+                print(e)
+            else:
+                sns.set(font_scale=1)
+            sns.boxplot(x=e, y=tgval, data=df, palette='pastel')
+            plt.savefig("../figure/boxPlot_" + datadesc + "_" + e +
+                        ".png", format="png", dpi=300)
+            # plt.show()
+
+    def _is_df_columns(self, df, col):
+        print("DataDescriptor()._is_df_columns(): Initialize")
+        result = True
+        for i, e in enumerate(col):
+            if not e in df.columns:
+                result = False
+        print("DataDescriptor()._is_df_columns(): return result=" + str(result))
+        return result
+
+    def test(self, train, test):
+        self.describe_column_specification(train, "train")
+        self.describe_column_specification(test, "test")
+        self.scatter_plot_multi(
+            train, ['odometer', 'year'], "price", "train")
+        self.box_plot_multi(
+            train, ['condition', 'cylinders', 'fuel', 'title_status', 'transmission', 'drive', 'size', 'type', 'paint_color'], "price", "train")
+        self.dist_plot_multi(
+            train, ['price', 'odometer', 'condition'], "price", "train")
+
+
+if __name__ == '__main__':
     main()
